eede5cc58d04e3cf313749633f9d3179e046d0aa
weed <weed_7777@yahoo.co.jp>
2012-08-12 19:21:47 +0900

    Initial Commit

---
+++ TouchAndGesture/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TouchAndGesture
+//
+//  Created by 達郎 植田 on 12/08/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TouchAndGesture/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TouchAndGesture
+//
+//  Created by 達郎 植田 on 12/08/12.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
